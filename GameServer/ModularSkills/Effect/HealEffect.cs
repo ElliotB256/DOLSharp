dfc352e38b7c1300791a9ac7d3d3fc3ffd6c1f2c
Unknown <robin.aakerman@gmail.com>
2017-12-05 21:50:38 +0100

    add HealEffect

---
+++ GameServer/ModularSkills/Effect/HealEffect.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DOL.GS.ModularSkills
+{
+    public class HealEffect : ISkillEffect, IEfficacious
+    {
+        public int Value { get; private set; }
+        public int Effectiveness { get; private set; }
+
+        int CalculateHeal() => Value * Effectiveness;
+
+        public bool Apply(GameObject recipient)
+        {
+            recipient.Health += CalculateHeal();
+            return true;
+        }
+
+        public void Expire(GameObject recipient)
+        {
+            // Heals are instant.
+        }
+    }
+}
