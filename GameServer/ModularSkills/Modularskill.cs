d2cb7f2a54481154b01b13d02b0a36f67f20b2dc
Elliot Bentine <ebentine@bitbucket.org>
2017-12-05 21:58:02 +0000

    Reorganised ModularSkill, started to implement event chaining so that skill users can listen to ModularSkill events

--- GameServer/ModularSkills/Modularskill.cs
+++ GameServer/ModularSkills/Modularskill.cs
@@ -6,9 +6,51 @@ namespace DOL.GS.ModularSkills
     /// <summary>
     /// Represents a skill composed of different modules
     /// </summary>
-    public class ModularSkill : IModularSkill
+    public class ModularSkill
     {
         private static readonly log4net.ILog Log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        /// <summary>
+        /// Target selector used for purposes of determining if the skill can be invoked.
+        /// </summary>
+        public ITargetSelector PrimaryTargetSelector
+        {
+            get
+            {
+                IList<SkillComponent> comps = Components;
+                if (comps.Count > 1)
+                    return comps[0].TargetSelector;
+                return null;
+            }
+        }
+
+        private IList<SkillComponent> m_components;
+        public IList<SkillComponent> Components
+        {
+            get { return m_components; }
+            set {
+                m_components = value;
+            }
+        }
+
+        public IModularSkillUser Owner { get; set; }
+
+        /// <summary>
+        /// Owner of the skill tries to use it
+        /// </summary>
+        public void TryUse()
+        {
+            if (Invocation == null)
+            {
+                Log.Error(string.Format("Could not start skill - null Invocation. skill Owner={0}", Owner));
+                return;
+            }
+            
+            Invocation.Start(Owner);
+        }
+
+
+        #region Invocation
 
         private ISkillInvocation m_invocation;
 
@@ -28,36 +70,14 @@ namespace DOL.GS.ModularSkills
         }
 
         /// <summary>
-        /// Target selector used for purposes of determining if the skill can be invoked.
+        /// The Modular skill is successfully invoked.
         /// </summary>
-        public ITargetSelector PrimaryTargetSelector
-        {
-            get
-            {
-                IList<SkillComponent> comps = Components;
-                if (comps.Count > 1)
-                    return comps[0].TargetSelector;
-                return null;
-            }
-        }
-
-        public IList<SkillComponent> Components { get; set; }
-
-        public IModularSkillUser Owner { get; set; }
+        public event EventHandler<SkillInvokedEventArgs> Invoked;
 
         /// <summary>
-        /// Owner of the skill tries to use it
+        /// The Modular skill could not be invoked because target requirements were not met
         /// </summary>
-        public void TryUse()
-        {
-            if (Invocation == null)
-            {
-                Log.Error(string.Format("Could not start skill - null Invocation. skill Owner={0}", Owner));
-                return;
-            }
-            
-            Invocation.Start(Owner);
-        }
+        public event EventHandler<FailSkillTargetRequirementsEventArgs> FailSkillTargetRequirement;
 
         /// <summary>
         /// Invoked when skill has successfully been cast.
@@ -79,10 +99,14 @@ namespace DOL.GS.ModularSkills
 
                 foreach (GameObject o in targets)
                 {
-                    sc.Applicator.Start(o, sc);
+                    sc.Applicator.Start(invoker, o, sc);
                 }
             }
+            var eh = Invoked;
+            eh?.Invoke(this, e);
         }
+
+        #endregion
 
         /// <summary>
         /// Invoked when skill effect chain is to be applied to target.
@@ -99,6 +123,5 @@ namespace DOL.GS.ModularSkills
                     break;
             }
         }
-        public delegate void OnSkillAppliedHandler(GameObject target, SkillComponent sc);
     }
 }
